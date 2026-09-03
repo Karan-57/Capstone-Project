bdbab4b709a03197d2fd863611ed9d5623ea925d
shravani9696 <shravanishinde8031@gmail.com>
2026-09-03 11:39:18 +0530

    fix repo merge conflicts

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,11 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
     
-const userModel = require('../models/user.model')
+const userModel = require('../model/user.model')
 const config = require('../config/config')
 const otpModel = require('../model/otp.model')
 const sessionModel = require('../model/session.model')
-const {sendEmail} = require('../services/email.sevice.js');
+const {sendEmail} = require('../services/email.service.js');
 // const blacklistModel = require('../models/blacklist.model')
 
 
