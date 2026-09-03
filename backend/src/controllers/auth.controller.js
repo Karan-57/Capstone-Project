f638dea1a76815d90629515b580efff3d3ae9741
shravani9696 <shravanishinde8031@gmail.com>
2026-09-03 16:21:55 +0530

    add refersh token and access token + modify auth Controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -6,7 +6,9 @@ const config = require('../config/config')
 const otpModel = require('../model/otp.model')
 const sessionModel = require('../model/session.model')
 const {sendEmail} = require('../services/email.service.js')
-const tokenBlacklistModel = require('../models/tokenBlacklist.model')
+const tokenBlacklistModel = require('../model/tokenBlacklist.model.js')
+
+import { generateOTP, generateOTPEmailHTML } from '../utils/util.js';
 
 
 
@@ -38,7 +40,7 @@ async function registerUserController(req,res){
         });
     }
 
-    const hashedPassword = bcrypt.hash(password,10);
+    const hashedPassword = await bcrypt.hash(password,10);
 
     const user = await userModel.create({
         username,
@@ -49,7 +51,7 @@ async function registerUserController(req,res){
     const otp = generateOTP();
     const html = generateOTPEmailHTML(otp);
 
-    const otpHash = bcrypt.hash(otp, 10);
+    const otpHash = await bcrypt.hash(otp,10    );
 
     await otpModel.create({
         email,
@@ -72,60 +74,75 @@ async function registerUserController(req,res){
 }
 
 
+
 /**
  * @name loginUserController
  * @description login a user, expect email and password
  * @access Public
  */
 
-async function loginUserController(req,res){
+async function loginUserController(req, res){
     const {email, password} = req.body;
-
-    if(!email || !password){
-        return res.status(400).json({
-            message:"email and password is required"
-        });
-    }
 
     const user = await userModel.findOne({email});
 
     if(!user){
         return res.status(401).json({
-            message:"Incorrect email or password"
+            message:"Invalid email or password"
         });
     }
+
+    if(!user.verified){
+        return res.status(401).json({
+            message:"user not verified"
+        });
+    }
+
+    const hashedPassword = await bcrypt.hash(password,10);
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if(!isPasswordValid){
-        return  res.status(401).json({
-            message:"Incorrect email or password"
+        return res.status(401).json({
+            message:"Invalid email or password"
         });
     }
 
-    const token = jwt.sign({
-        id:user._id,
-        username:user.username
-    },process.env.JWT_SECRET,{
-        expiresIn:"1d"
+    const refreshToken = jwt.sign({
+        id:user._id
+    },config.JWT_SECRET,{
+        expiresIn:'7d'
     });
 
-    res.cookie("token", token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        maxAge: 24 * 60 * 60 * 1000
+    const refreshTokenHash = await bcrypt.hash(refreshToken,10);
+
+    const session = await sessionModel.create({
+        user:user._id,
+        refreshTokenHash,
+        ip:req.ip,
+        userAgent: req.headers['user-agent']
+    });
+
+    const accessToken = jwt.sign({
+        id:user._id,
+        session:session._id
+    },config.JWT_SECRET,{
+        expiresIn:'15m'
+    });
+
+    res.cookie('refreshToken',refreshToken,{
+        httpOnly:true,
+        secure:true,
+        sameSite:"strict",
+        maxAge:7*24*60*60*1000
     });
 
     res.status(200).json({
         message:"user logged in",
-        user:{
-            id:user._id,
-            username:user.username,
-            email:user.email
-        }
+        accessToken
     });
 }
+
 
 
 /**
@@ -133,7 +150,7 @@ async function loginUserController(req,res){
  * @description logout a user and then add token to blacklist
  * @access Public
  */
-async function logoutUser(req, res){ 
+async function logoutUserController(req, res){ 
     
     const refreshToken = req.cookies.refreshToken;
 
@@ -141,7 +158,7 @@ async function logoutUser(req, res){
         return res.status(400).json({message:"refresh token not found"});
     }
 
-    const refreshTokenHash = await bcrypt.hash(refreshToken,10);
+    const refreshTokenHash = await bcrypt.hash(refreshToken);
 
     const session = await sessionModel.findOne({
         refreshTokenHash,
@@ -163,22 +180,22 @@ async function logoutUser(req, res){
         message:"user logged out successfully"
     });
 }
-
 /**
  * @name getMeController
  * @description to get current user info
  * @access Public
  */
 async function getMeController(req,res){
-    const user = await userModel.findById(req.user.id);
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if(!token){
+        return res.status(401).json({message:"token not found"});
+    }
+
+    const decoded = jwt.verify(token, config.JWT_SECRET);
 
     res.status(200).json({
-        message:"user details fetched sucessfully",
-        user:{
-            id:user._id,
-            email:user.email,
-            username: user.username
-        }
+        decoded
     });
 }
 
